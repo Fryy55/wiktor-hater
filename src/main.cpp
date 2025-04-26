6a7ab3fc8ac6e797659ff6399905f02188c3fba1
Fryy55 <ivan_ugu55@icloud.com>
2025-04-27 02:25:48 +0500

    Finish all features so far idk

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ using namespace dpp;
 
 
 int main() {
-	cluster bot(BOT_TOKEN);
+	cluster bot(BOT_TOKEN, i_default_intents | i_message_content);
 	bot.on_log(utility::cout_logger());
 
 	bot.on_ready([&bot](ready_t const& event) {
@@ -22,12 +22,38 @@ int main() {
 		bot.set_presence(presence(ps_online, at_watching, "wiktor's downfall!"));
 	});
 
-	bot.on_slashcommand([&bot](slashcommand_t const& event) -> task<void> {
+	bot.on_slashcommand([&bot](slashcommand_t const& event) {
 		if (event.command.get_command_name() == "hate-wik")
-			event.reply(message().add_file("kill.jpg", "C:\\Users\\User\\Pictures\\Camera Roll\\kill.jpg"));
+			event.reply(message().add_file("kill.jpg", utility::read_file("C:\\Users\\User\\Pictures\\Camera Roll\\kill.jpg")));
 	});
 
-	bot.start(dpp::st_wait);
+	bot.on_message_create([&bot](message_create_t const& event) {
+		if (event.msg.author.username == "fryy_55") {
+			bot.message_add_reaction(event.msg, "❌");
+			return;
+		}
+
+
+		bool mentionedWik = false;
+		for (auto ping : event.msg.mentions)
+			if (mentionedWik = ping.first.username == "wdwiktor")
+				break;
+
+		if (
+			event.msg.author.format_username() != "wiktor hater#7539"
+			&&
+			(
+				lowercase<char>(event.msg.content).find("wik") != event.msg.content.npos
+				||
+				lowercase<char>(event.msg.content).find("wiktor") != event.msg.content.npos
+				||
+				mentionedWik
+			)
+		)
+			event.reply(std::string("wiktor mentioned ") + (mentionedWik ? "(literally) " : "") + " :thumbsdown:");
+	});
+
+	bot.start(st_wait);
 
 	return 0;
 }
